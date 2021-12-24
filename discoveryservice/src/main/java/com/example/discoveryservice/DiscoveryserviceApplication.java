abfa32b19276c1de70464dd42273b61df7068db3
YoungKyonYou <man.spride1324@gmail.com>
2021-12-25 00:02:57 +0900

    initial commit

---
+++ discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -0,0 +1,16 @@
+package com.example.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//유레카 서버로서 동작하기 위해서는 서버의 자격을 줘야하는데 이게 바로 EnableEurekaServer 어노테이션이다.
+@EnableEurekaServer
+public class DiscoveryserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryserviceApplication.class, args);
+    }
+
+}
