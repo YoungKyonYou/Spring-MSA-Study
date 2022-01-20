dcee4003d16065dcc10fb717b3b2cad39b8246c1
YoungKyonYou <man.spride1324@gmail.com>
2022-01-21 00:35:09 +0900

    update

--- E-Commerce/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ E-Commerce/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,13 +1,26 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.error.FeignErrorDecoder;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.Feign;
+import feign.FeignException;
+import feign.Response;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
+
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,16 +28,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
     final private UserRepository userRepository;
     final private BCryptPasswordEncoder passwordEncoder;
+    final private RestTemplate restTemplate;
+    final private Environment env;
+    final private OrderServiceClient orderServiceClient;
+    final private FeignErrorDecoder feignErrorDecoder;
+    final private CircuitBreakerFactory circuitBreakerFactory;
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -53,8 +73,35 @@ public class UserServiceImpl implements UserService {
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
+//        List<ResponseOrder> orders = new ArrayList<>();
+
+        /*Using as rest template*/
+//        String orderUrl=String.format(env.getProperty("order_service.url"),userId);
+//        ResponseEntity<List<ResponseOrder>> orderListResponse=restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+//                new ParameterizedTypeReference<List<ResponseOrder>>() {
+//        });
+//
+//        List<ResponseOrder> orderList=orderListResponse.getBody();
+
+        /* Using Feign Client*/
+        /* Feign exception handling*/
+//        List<ResponseOrder> orderList=null;
+//        try{
+//            orderList=orderServiceClient.getOrders(userId);
+//        }catch(FeignException ex){
+//            log.error(ex.getMessage());
+//        }
+
+        /*ErrorDecoder*/
+//        List<ResponseOrder> orderList=orderServiceClient.getOrders(userId);
+
+        log.info("Before call orders microservice");
+        CircuitBreaker circuitBreaker=circuitBreakerFactory.create("circuitbreaker");
+        //문제가 생길 시 빈 리스트 반환
+        List<ResponseOrder> orderList=circuitBreaker.run(()->orderServiceClient.getOrders(userId),
+                throwable->new ArrayList<>());
+        log.info("After call orders microservice");
+        userDto.setOrders(orderList);
 
         return userDto;
     }
